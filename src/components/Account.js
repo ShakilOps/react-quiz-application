fd57e4c7d63e1c650fd8a578803044adbec14e95
ShakilOps <shakildo.7887@gmail.com>
2021-09-26 03:09:33 +0600

    end-markup

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,15 +1,15 @@
-import classes from '../styles/Account.module.css';
+import { Link } from "react-router-dom";
+import classes from "../styles/Account.module.css";
 
 export default function Account() {
   return (
-    <div>
-      <div className={classes.account}>
-        <span className="material-icons-outlined" title="Account">
-          account_circle
-        </span>
-        <a href="signup.html">Signup</a>
-        {/* <!-- <span class="material-icons-outlined" title="Logout"> logout </span> --> */}
-      </div>
+    <div className={classes.account}>
+      <span className="material-icons-outlined" title="Account">
+        account_circle
+      </span>
+      <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+      {/* <span className="material-icons-outlined" title="Logout"> logout </span> */}
     </div>
-  )
+  );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import Videos from "../Videos";
 
 export default function Home() {
-    return (
-        <div>
-            Hello Lady
-        </div>
-    )
+  return <Videos />;
 }
